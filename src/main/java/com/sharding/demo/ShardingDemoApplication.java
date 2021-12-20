c65bb077e45aa415d687c0b311e3832e9bb43a4f
麻绛 <majiang213@foxmail.com>
2021-12-20 16:12:22 +0800

    晚上提交配置

--- src/main/java/com/sharding/demo/ShardingDemoApplication.java
+++ src/main/java/com/sharding/demo/ShardingDemoApplication.java
@@ -1,11 +1,13 @@
 package com.sharding.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.sharding")
+@MapperScan(basePackages = "com.sharding.demo.dao")
 public class ShardingDemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/sharding/demo/service/KeyTestService.java
+++ src/main/java/com/sharding/demo/service/KeyTestService.java
@@ -1,10 +1,13 @@
 package com.sharding.demo.service;
 
 import com.sharding.demo.dao.KeyTestDao;
+import org.apache.shardingsphere.api.hint.HintManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class KeyTestService {
 
     @Autowired
@@ -12,5 +15,20 @@ public class KeyTestService {
 
     public Integer getList() {
         return keyTestDao.getList();
+    }
+
+    public void InterAfterGet() {
+        keyTestDao.insert();
+        System.out.println(keyTestDao.getList());
+    }
+
+    public void getAfterInsert() {
+        try (HintManager instance = HintManager.getInstance()) {
+            instance.setMasterRouteOnly();
+            System.out.println(keyTestDao.getList());
+            keyTestDao.insert();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
